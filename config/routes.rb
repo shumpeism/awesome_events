eb31f38183a183c1f31eeea4bd6bf74786cf6690
Shumpei Kikuchi <shun.ynu@gmail.com>
2015-04-06 15:48:22 +0900

    change OAuth

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   get '/auth/:provider/callback' => 'sessions#create'
+
   get '/logout' => 'sessions#destroy', as: :logout
 
   resource :user do
